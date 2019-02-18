955954b15723dddaac72bc66176a740e60069c8f
lopdam <dennyslopez2013@gmail.com>
2019-02-18 16:47:35 -0500

    Hola Mundo

---
+++ HolaMundo/HolaMundo.ino
@@ -0,0 +1,23 @@
+//lopdam
+
+//Vamos a Realizar el Hola Mudno en la Plataforma de Arduino
+
+// El pin 13 ya lleva por defecto integrado un led
+const int hola=13;
+const int tim=1000;
+
+void setup(){
+  // Declaramos el modo del pin 13
+  pinMode(hola,OUTPUT);
+
+}
+
+void loop(){
+//Encendemos el pin 13 por un momento
+digitalWrite(hola,HIGH);
+delay(tim);
+//Apagamos el pin 13 por un momento
+digitalWrite(hola,LOW);
+delay(tim);
+
+}
