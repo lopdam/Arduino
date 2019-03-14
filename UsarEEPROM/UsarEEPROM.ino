7a107721bf86c11f315e8087e56edeacdd34595d
roboto <roboto@debian.lopdam.com>
2019-03-14 02:01:42 -0500

    Usar EEPROM

---
+++ UsarEEPROM/UsarEEPROM.ino
@@ -0,0 +1,26 @@
+//lopdam
+//Usar memoria EEPROM== 1kB
+//Total 1000 Direcciones de memoria de 0-999
+
+//importamos la libreria
+#include <EEPROM.h>
+//Metodos
+//EEPROM.write(Direccion,Informacion)-> Informacion va hasta 8 bits==1byte
+// int 0-255
+
+//EEPROM.read(Direccion)
+
+
+byte i;
+
+int k=10;
+
+void setup() {
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+
+}
